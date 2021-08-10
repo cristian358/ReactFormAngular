aeff8b7b953081fe7f3eacc2f4243f7661fac3f0
cristian358 <83902584+cristian358@users.noreply.github.com>
2021-08-10 18:01:04 +0300

    Adding angular material
    1. Adding angular material
    2. Fixing issues

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,80 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms'
+import { Interface } from 'readline';
+import {Observable} from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
 
+export interface City {
+  name: string;
+  region: Region;
+}
+
+export interface Region {
+  name: string;
+  postalcode: string;
+  adress: Array<Adress>;
+}
+
+// export interface MyInterface extends Array<Adress> { }
+
+export interface Adress {
+  streetname: string;
+  housenumber: number;
+}
+/**
+ * @title Autocomplete overview
+ */
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'React Forms'; 
-  formdata = new FormGroup({ 
-    userName: new FormControl("ReactFormExample"),
-    firstname: new FormControl(),
-    lastname: new FormControl(),
-    gender: new FormControl(),
+  stateCtrl = new FormControl();
+  filteredStates: Observable<City[]>;
 
- }); 
 
-  onSubmit() {
-    console.log(this.formdata.value);
+  states: City[] = [
+    {
+      name: 'MD-2536',
+      region : {
+        name: "aaaa",
+        postalcode: "MD-3654",
+        adress: [{
+          streetname: "Alba Iulia",
+          housenumber: 35
+        }]
+      }
+    },
+    {
+      name: 'MD-2537',
+      region : {
+        name: "aaaa",
+        postalcode: "MD-3654",
+        adress: [{
+          streetname: "Alba Iulia",
+          housenumber: 35
+        }]
+      }
+    }
+  ];
+
+  constructor() {
+    this.filteredStates = this.stateCtrl.valueChanges
+      .pipe(
+        startWith(''),
+        map(state => state ? this._filterStates(state) : this.states.slice())
+      );
+  }
+
+  private _filterStates(value: string): City[] {
+    const filterValue = value.toLowerCase();
+
+    return this.states.filter(state => state.name.toLowerCase().includes(filterValue));
   }
 }
+
+
+/**  Copyright 2021 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,36 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {NgModule} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatNativeDateModule} from '@angular/material/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {DemoMaterialModule} from '..//app/material-module';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    ReactiveFormsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    DemoMaterialModule,
+    MatNativeDateModule,
+    ReactiveFormsModule,
+    AppRoutingModule
   ],
-  providers: [],
+  entryComponents: [AppComponent],
+  declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
+
+
+/**  Copyright 2021 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
